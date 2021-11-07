8926575a389ca43cb8963659f84b689ee075dcd8
RememBerBer <rememberber@163.com>
2021-11-07 17:12:38 +0800

    init

---
+++ src/main/java/com/luoboduner/moo/info/App.java
@@ -0,0 +1,10 @@
+package com.luoboduner.moo.info;
+
+/**
+ * Main Enter!
+ *
+ * @author <a href="https://github.com/rememberber">RememBerBer</a>
+ * @since 2021/11/07.
+ */
+public class App {
+}
---
+++ src/test/java/com/luoboduner/moo/info/AppTest.java
@@ -0,0 +1,18 @@
+package com.luoboduner.moo.info;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
