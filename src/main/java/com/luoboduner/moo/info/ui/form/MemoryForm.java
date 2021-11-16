e14fbd705cfe8eba3698a53f864e394a7464d523
RememBerBer <rememberber@163.com>
2021-11-16 20:16:16 +0800

    cpu and memory form added

--- src/main/java/com/luoboduner/moo/info/ui/form/MemoryForm.java
+++ src/main/java/com/luoboduner/moo/info/ui/form/MemoryForm.java
@@ -2,11 +2,18 @@ package com.luoboduner.moo.info.ui.form;
 
 import cn.hutool.log.Log;
 import cn.hutool.log.LogFactory;
+import com.intellij.uiDesigner.core.GridConstraints;
 import com.intellij.uiDesigner.core.GridLayoutManager;
+import com.intellij.uiDesigner.core.Spacer;
+import com.luoboduner.moo.info.App;
 import lombok.Getter;
+import oshi.hardware.GlobalMemory;
+import oshi.hardware.PhysicalMemory;
+import oshi.hardware.VirtualMemory;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.List;
 
 /**
  * MemoryForm
@@ -20,6 +27,14 @@ public class MemoryForm {
 
     private static MemoryForm memoryForm;
     private JPanel mainPanel;
+    private JProgressBar pysicalMemoryProgressBar;
+    private JProgressBar virtualMemoryProgressBar;
+    private JLabel pysicalMemoryTitle;
+    private JLabel pysicalMemoryUsed;
+    private JLabel pysicalMemoryAvailable;
+    private JLabel virtualMemoryTitle;
+    private JLabel virtualMemoryUsed;
+    private JLabel virtualMemoryAvailable;
 
     public static MemoryForm getInstance() {
         if (memoryForm == null) {
@@ -39,6 +54,10 @@ public class MemoryForm {
     }
 
     private static void initInfo() {
+        GlobalMemory memory = App.si.getHardware().getMemory();
+
+        List<PhysicalMemory> physicalMemory = memory.getPhysicalMemory();
+        VirtualMemory virtualMemory = memory.getVirtualMemory();
     }
 
     {
@@ -58,6 +77,45 @@ public class MemoryForm {
     private void $$$setupUI$$$() {
         mainPanel = new JPanel();
         mainPanel.setLayout(new GridLayoutManager(1, 1, new Insets(0, 0, 0, 0), -1, -1));
+        final JScrollPane scrollPane1 = new JScrollPane();
+        mainPanel.add(scrollPane1, new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_WANT_GROW, null, null, null, 0, false));
+        final JPanel panel1 = new JPanel();
+        panel1.setLayout(new GridLayoutManager(3, 1, new Insets(0, 0, 0, 0), -1, -1));
+        scrollPane1.setViewportView(panel1);
+        final JPanel panel2 = new JPanel();
+        panel2.setLayout(new GridLayoutManager(3, 3, new Insets(0, 0, 0, 0), -1, -1));
+        panel1.add(panel2, new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
+        pysicalMemoryTitle = new JLabel();
+        pysicalMemoryTitle.setText("Pysical Memory");
+        panel2.add(pysicalMemoryTitle, new GridConstraints(0, 0, 1, 3, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        pysicalMemoryProgressBar = new JProgressBar();
+        panel2.add(pysicalMemoryProgressBar, new GridConstraints(1, 0, 1, 3, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        pysicalMemoryUsed = new JLabel();
+        pysicalMemoryUsed.setText("Used -");
+        panel2.add(pysicalMemoryUsed, new GridConstraints(2, 0, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        pysicalMemoryAvailable = new JLabel();
+        pysicalMemoryAvailable.setText("- Available");
+        panel2.add(pysicalMemoryAvailable, new GridConstraints(2, 2, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        final Spacer spacer1 = new Spacer();
+        panel2.add(spacer1, new GridConstraints(2, 1, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, 1, null, null, null, 0, false));
+        final JPanel panel3 = new JPanel();
+        panel3.setLayout(new GridLayoutManager(3, 3, new Insets(0, 0, 0, 0), -1, -1));
+        panel1.add(panel3, new GridConstraints(1, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
+        virtualMemoryTitle = new JLabel();
+        virtualMemoryTitle.setText("Virtual Memory(Swap)");
+        panel3.add(virtualMemoryTitle, new GridConstraints(0, 0, 1, 3, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        virtualMemoryProgressBar = new JProgressBar();
+        panel3.add(virtualMemoryProgressBar, new GridConstraints(1, 0, 1, 3, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        virtualMemoryUsed = new JLabel();
+        virtualMemoryUsed.setText("Used -");
+        panel3.add(virtualMemoryUsed, new GridConstraints(2, 0, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        virtualMemoryAvailable = new JLabel();
+        virtualMemoryAvailable.setText("- Available");
+        panel3.add(virtualMemoryAvailable, new GridConstraints(2, 2, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        final Spacer spacer2 = new Spacer();
+        panel3.add(spacer2, new GridConstraints(2, 1, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, 1, null, null, null, 0, false));
+        final Spacer spacer3 = new Spacer();
+        panel1.add(spacer3, new GridConstraints(2, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_VERTICAL, 1, GridConstraints.SIZEPOLICY_WANT_GROW, null, null, null, 0, false));
     }
 
     /**
@@ -66,4 +124,5 @@ public class MemoryForm {
     public JComponent $$$getRootComponent$$$() {
         return mainPanel;
     }
+
 }
