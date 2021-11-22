e88890c05cfda11bf2eb31afc21fc3f030e22249
RememBerBer <rememberber@163.com>
2021-11-22 13:51:08 +0800

    power source first step

--- src/main/java/com/luoboduner/moo/info/ui/form/PowerSourceForm.java
+++ src/main/java/com/luoboduner/moo/info/ui/form/PowerSourceForm.java
@@ -92,6 +92,11 @@ public class PowerSourceForm {
             powerPanel.add(capacityLabel, new GridConstraints(2, 0, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
 
             JLabel temperatureLabel = new JLabel();
+            double powerUsageRate = powerSource.getPowerUsageRate();
+            double amperage = powerSource.getAmperage();
+            double voltage = powerSource.getVoltage();
+            boolean charging = powerSource.isCharging();
+            int cycleCount = powerSource.getCycleCount();
             temperatureLabel.setText(String.valueOf(String.format("Temperature: %.1f°C", powerSource.getTemperature())));
             powerPanel.add(temperatureLabel, new GridConstraints(2, 2, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
 
