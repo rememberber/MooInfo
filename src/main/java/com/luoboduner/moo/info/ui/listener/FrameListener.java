e5747803791d365977422e75933dcef2a3f286e9
rememberber <rememberber@163.com>
2023-09-08 21:59:45 +0800

    Window:double click event to max or normal

--- src/main/java/com/luoboduner/moo/info/ui/listener/FrameListener.java
+++ src/main/java/com/luoboduner/moo/info/ui/listener/FrameListener.java
@@ -6,6 +6,8 @@ import com.luoboduner.moo.info.ui.Init;
 import com.luoboduner.moo.info.util.SystemUtil;
 
 import javax.swing.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
 
@@ -59,6 +61,39 @@ public class FrameListener {
 
             @Override
             public void windowActivated(WindowEvent e) {
+
+            }
+        });
+
+        App.mainFrame.addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                if (e.getClickCount() == 2 && !e.isConsumed()) {
+                    if (App.mainFrame.getExtendedState() == JFrame.MAXIMIZED_BOTH) {
+                        App.mainFrame.setExtendedState(JFrame.NORMAL);
+                    } else {
+                        App.mainFrame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+                    }
+                }
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
 
             }
         });
